211ff6377d6fa46b53ad928b045363d7f61b1b9f
Anthony Gelibert <anthony.gelibert@me.com>
2011-01-24 15:12:29 +0100

    Update Copyright and add some marks

--- cpu/msp430f5x/msp430.h
+++ cpu/msp430f5x/msp430.h
@@ -1,34 +1,22 @@
-/*
- * Copyright (c) 2007, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- * $Id: msp430.h,v 1.4 2011/01/05 13:36:38 joxe Exp $
+/******************************************************************************
+ * Copyright 2010, Plateforme Technologique de Valence                        *
+ *                                                                            *
+ *    Licensed under the Apache License, Version 2.0 (the "License");         *
+ *    you may not use this file except in compliance with the License.        *
+ *    You may obtain a copy of the License at                                 *
+ *                                                                            *
+ *        http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                            *
+ *    Unless required by applicable law or agreed to in writing, software     *
+ *    distributed under the License is distributed on an "AS IS" BASIS,       *
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ *    See the License for the specific language governing permissions and     *
+ *    limitations under the License.                                          *
+ ******************************************************************************/
+/**
+ * \author Anthony Gelibert and Fabien Rey
+ * \date Jan 24, 2010
+ * \version 0.0.2
  */
 
 #ifndef __MSP430_H__
@@ -48,6 +36,8 @@
 #define MSP430_CPU_SPEED 2457600UL
 #endif
 
+#pragma mark LPM Macro
+
 /** NOT_YET_DOCUMENTED_PTV */
 #define MSP430_REQUIRE_CPUON 0
 /** NOT_YET_DOCUMENTED_PTV */
@@ -56,7 +46,6 @@
 #define MSP430_REQUIRE_LPM2 2
 /** NOT_YET_DOCUMENTED_PTV */
 #define MSP430_REQUIRE_LPM3 3
-
 /**
  * NOT_YET_DOCUMENTED_PTV
  *
@@ -64,12 +53,21 @@
  */
 #define MSP430_REQUIRE_LPM4 4
 
+
+#pragma mark LPM Functions
+
 /**
  * NOT_YET_DOCUMENTED_PTV
  *
  * @param req NOT_YET_DOCUMENTED_PTV
  */
 PUBLIC void msp430_add_lpm_req(int req);
+
+/**
+ * NOT_YET_DOCUMENTED_PTV
+ *
+ * @param req NOT_YET_DOCUMENTED_PTV
+ */
 PUBLIC void msp430_remove_lpm_req(int req);
 
 #endif /* __MSP430_H__ */
