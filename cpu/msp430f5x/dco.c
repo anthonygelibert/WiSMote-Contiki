a6f5912eab9ac8daf27bb47ae46701a453df0881
Fabien Rey <fabien-rey@wanadoo.fr>
2011-02-03 17:17:46 +0100

    Use XT1

--- cpu/msp430f5x/dco.c
+++ cpu/msp430f5x/dco.c
@@ -48,144 +48,27 @@ static int msp430_dco_required;
 void
 msp430_init_dco(void)
 {
-  /* This code taken from the FU Berlin sources and reformatted. */
-  unsigned int DELTA = ((MSP430_CPU_SPEED) / (32768 / 8));
+  P4DIR |= 0x80;
+  P4SEL |= 0x80;
 
-  unsigned int compare, oldcapture = 0;
-  unsigned int i;
+  // Activate XT1
+  P7SEL |= BIT0 + BIT1;
+  // Set XT1 On
+  UCSCTL6 &= ~XT1OFF;
+  // Max drive strength, adjust according to crystal frequency. LFXT1 HF mode
+  UCSCTL6 |= XT1DRIVE_2 + XTS + XT2OFF;
 
-  // ACLK is divided by 4. RSEL=6 no division for MCLK and SSMCLK. XT2 is off. */
-  //BCSCTL1 = 0xa4;
+  do
+    {
+      UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + XT1HFOFFG + DCOFFG);
+                                              // Clear XT2,XT1,DCO fault flags
+      SFRIFG1 &= ~OFIFG;                      // Clear fault flags
+    }while (SFRIFG1&OFIFG);                   // Test oscillator fault flag
 
-  /* XXX_PTV CHOOSE THE GOOD VALUE HERE. */
-  /* <=> RSEL = x */
-  UCSCTL1 |= DCORSEL_2;
-  /* <=> XT2 and XT1 is off */
-  UCSCTL6 |= XT2OFF | XT1OFF;
-  /* <=> XTS Low-frequency */
-  UCSCTL6 &= ~XTS;
-  /* <=> ACLK/8 - MCLK/1 - SMCLK/1 */
-  UCSCTL5 = DIVA__8 | DIVS__1 | DIVM__1;
+  UCSCTL4 = SELA_0 + SELS_0 + SELM_0;       // MCLCK = XT1 , SMCLK = XT1 , ACLK = XT1
+  UCSCTL5 |= DIVA__4 + DIVS__4 + DIVM__4;// Set Clock divider 4 -> Clock = 4MHz
 
-  /* Init FLL to desired frequency using the 32762Hz crystal DCO frequency = 2,4576 MHz  */
-  //BCSCTL2 = 0x00;
-  /* <=> ACLK = DCO - SMCLK = DCO - MCLK = DCO */
-  UCSCTL4 = SELA__DCOCLK | SELS__DCOCLK |  SELM__DCOCLK;
 
-  // BCSCTL1 |= DIVA1 + DIVA0; /* ACLK = LFXT1CLK/8 */
-  for (i = 0xffff; i > 0; i--) { /* Delay for XTAL to settle */
-    asm("nop");
-  }
-
-  //CCTL2 = CCIS0 + CM0 + CAP; // Define CCR2, CAP, ACLK
-  TA1CCTL2 = CCIS_0 | CM_0 | CAP | CCIE;
-
-  //  TACTL = TASSEL1 + TACLR + MC1; // SMCLK, continuous mode
-  /* <=> SMCLK / CLEAR / CONTINOUS */
-  TA1CTL |= TASSEL__SMCLK | TACLR | MC__CONTINOUS;
-
-  while (1) {
-    // Wait until capture occurred!
-    while (TA1CCTL2 & CCIFG) {
-    }
-
-    TA1CCTL2 &= ~CCIFG; // Capture occurred, clear flag
-    compare = TA1CCR2; // Get current captured SMCLK
-    compare -= oldcapture; // SMCLK difference
-    oldcapture = TA1CCR2; // Save current captured SMCLK
-
-    if (DELTA == compare) {
-      break; /// if equal, leave "while(1)"
-    } else {
-      if (DELTA < compare) { /// DCO is too fast, slow it down
-        //        DCOCTL--;
-        UCSCTL0_H--;
-        if (UCSCTL0_H == 0x1F) { // Did DCO role under?
-          UCSCTL1_L = (UCSCTL1_L & ~DCORSEL_7) | (((UCSCTL1_L >> 4) - 1) << 4);
-        }
-      } else { // -> Select next lower RSEL
-        UCSCTL0_H++;
-        if (UCSCTL0_H == 0x00) { // Did DCO role over?
-          UCSCTL1_L = (UCSCTL1_L & ~DCORSEL_7) | (((UCSCTL1_L >> 4) + 1) << 4);
-        }
-        // -> Select next higher RSEL
-      }
-    }
-  }
-
-  TA1CCTL2 = 0; /* Stop CCR2 function */
-  TA1CTL = 0; /* Stop Timer_A */
-
-  //  BCSCTL1 &= ~(DIVA1 + DIVA0); /* remove /8 divisor from ACLK again */
-  UCSCTL5 &= ~(DIVA__8);
-}
-
-/** Synchronize DCO.
- *
- *  \note this code will always start the TimerB if not already started
- */
-void
-msp430_sync_dco(void)
-{
-  uint16_t last;
-  uint16_t diff;
-
-  /* DELTA_2 assumes an ACLK of 32768 Hz */
-#define DELTA_2    ((MSP430_CPU_SPEED) / 32768)
-
-  /* Select SMCLK clock, and capture on ACLK for TBCCR6 */
-  TBCTL = TBSSEL1 | TBCLR;
-  TBCCTL6 = CCIS0 + CM0 + CAP;
-  /* start the timer */
-  TBCTL |= MC1;
-
-  // wait for next Capture
-  TBCCTL6 &= ~CCIFG;
-  while (!(TBCCTL6 & CCIFG))
-    ;
-  last = TBCCR6;
-
-  TBCCTL6 &= ~CCIFG;
-  // wait for next Capture - and calculate difference
-  while (!(TBCCTL6 & CCIFG))
-    ;
-  diff = TBCCR6 - last;
-
-  /* Stop timer - conserves energy according to user guide */
-  TBCTL = 0;
-
-  /*   speed = diff; */
-  /*   speed = speed * 32768; */
-  /*   printf("Last TAR diff:%d target: %ld ", diff, DELTA_2); */
-  /*   printf("CPU Speed: %lu DCOCTL: %d\n", speed, DCOCTL); */
-
-  /* resynchronize the DCO speed if not at target */
-  /*  if (DELTA_2 < diff) {
-   DCOCTL--;
-   if (DCOCTL == 0xFF) {
-   BCSCTL1--;
-   }
-   } else
-   if (DELTA_2 > diff) {
-   DCOCTL++;
-   if (DCOCTL == 0x00) {
-   BCSCTL1++;
-   }
-   }*/
-  if (DELTA_2 < diff) { /* DCO is too fast, slow it down */
-    //        DCOCTL--;
-    UCSCTL0_H--;
-    if (UCSCTL0_H == 0x1F) { /* Did DCO role under? */
-      UCSCTL1_L = (UCSCTL1_L & ~DCORSEL_7) | (((UCSCTL1_L >> 4) - 1) << 4);
-    }
-  } else {
-    if (DELTA_2 > diff) { /* -> Select next lower RSEL */
-      UCSCTL0_H++;
-      if (UCSCTL0_H == 0x00) { /* Did DCO role over? */
-        UCSCTL1_L = (UCSCTL1_L & ~DCORSEL_7) | (((UCSCTL1_L >> 4) + 1) << 4);
-      }
-    }
-  }
 }
 
 /** Increment the msp430_dco_required value. */
