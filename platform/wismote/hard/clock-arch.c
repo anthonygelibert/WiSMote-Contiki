32e1bfb0ea7f139bb4d3851cebdd58e00b7b389d
Anthony Gelibert <anthony.gelibert@me.com>
2011-02-04 10:44:53 +0100

    Add DEBUG_CLOCK symbol

--- platform/wismote/hard/clock-arch.c
+++ platform/wismote/hard/clock-arch.c
@@ -1,9 +1,17 @@
+/** NOT_YET_DOCUMENTED_PTV : Licence */
 #include <contiki-conf.h>
 
+#define DEBUG_CLOCK
+
+/**
+ * NOT_YET_DOCUMENTED_PTV
+ */
 void clock_platform_init(void)
 {
+#ifdef DEBUG_CLOCK
   P4DIR |= 0x80;
   P4SEL |= 0x80;
-
+#endif
+  /* XT1 */
   P7SEL |= BIT0 + BIT1;
 }
