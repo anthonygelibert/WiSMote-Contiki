5ec20a028031beccb0d3de09f49a26a0b9406de8
Anthony Gelibert <anthony.gelibert@me.com>
2011-02-10 10:53:17 +0100

    Process name and coding style

--- examples/leds/leds_test.c
+++ examples/leds/leds_test.c
@@ -1,7 +1,38 @@
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
+/*
+ * Copyright (c) 2011, Plateforme Technologique de Valence.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
+
+/**
+ * \author Anthony Gelibert
+ * \date Feb 10, 2011
+ * \version 0.0.2
+ */
+
 #include "contiki.h"
 #include "sys/process.h"
 #include "sys/autostart.h"
@@ -10,31 +41,31 @@
 
 #include <stdint.h>
 
-
-void delay(void) {
-    volatile uint16_t i;
-    for (i = 0; i < 64000; i++) {
-            asm volatile("nop");
-    }
+void
+delay(void)
+{
+  volatile uint16_t i;
+  for (i = 0; i < 64000; i++) {
+    asm volatile("nop");
+  }
 }
 
 /*---------------------------------------------------------------------------*/
-PROCESS(hello_world_process, "Hello world process");
-AUTOSTART_PROCESSES(&hello_world_process);
+PROCESS(leds_process, "LEDs process");
+AUTOSTART_PROCESSES(&leds_process);
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(hello_world_process, ev, data)
+PROCESS_THREAD(leds_process, ev, data)
 {
   PROCESS_BEGIN();
 
   watchdog_stop();
 
- while (1) {
-  leds_off(LEDS_BLUE | LEDS_RED | LEDS_GREEN);
-  delay();
-  leds_on(LEDS_BLUE | LEDS_RED | LEDS_GREEN);
-  delay();
+  while (1) {
+    leds_off(LEDS_BLUE | LEDS_RED | LEDS_GREEN);
+    delay();
+    leds_on(LEDS_BLUE | LEDS_RED | LEDS_GREEN);
+    delay();
   }
-
 
   PROCESS_END();
 }
