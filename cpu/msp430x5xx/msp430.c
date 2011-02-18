0c251f3c180a90c6ed41170a98c55a832db16b62
Anthony Gelibert <anthony.gelibert@me.com>
2011-02-18 22:01:42 +0100

    Initialize all the ports as output

--- cpu/msp430x5xx/msp430.c
+++ cpu/msp430x5xx/msp430.c
@@ -36,6 +36,7 @@
 #include "msp430.h"
 #include "dev/watchdog.h"
 
+#include "contiki-conf.h"
 #include <signal.h>
 
 /** Ports initialization.
@@ -51,6 +52,7 @@
 static void
 init_ports(void)
 {
+  /* See MSP430x5xx/MSP430x6xx Family p.320 */
   /* All configured for digital I/O */
 #ifdef P1SEL
   P1SEL = 0;
@@ -77,38 +79,30 @@ init_ports(void)
   P8SEL = 0;
 #endif
 
-  /* All available inputs */
+  /* All available outputs */
 #ifdef P1DIR
-  P1DIR = 0;
-  P1OUT = 0;
+  P1DIR = 0xFF;
 #endif
 #ifdef P2DIR
-  P2DIR = 0;
-  P2OUT = 0;
+  P2DIR = 0xFF;
 #endif
 #ifdef P3DIR
-  P3DIR = 0;
-  P3OUT = 0;
+  P3DIR = 0xFF;
 #endif
 #ifdef P4DIR
-  P4DIR = 0;
-  P4OUT = 0;
+  P4DIR = 0xFF;
 #endif
 #ifdef P5DIR
-  P5DIR = 0;
-  P5OUT = 0;
+  P5DIR = 0xFF;
 #endif
 #ifdef P6DIR
-  P6DIR = 0;
-  P6OUT = 0;
+  P6DIR = 0xFF;
 #endif
 #ifdef P7DIR
-  P7DIR = 0;
-  P7OUT = 0;
+  P7DIR = 0xFF;
 #endif
 #ifdef P8DIR
-  P8DIR = 0;
-  P8OUT = 0;
+  P8DIR = 0xFF;
 #endif
 
   /* All available IE */
