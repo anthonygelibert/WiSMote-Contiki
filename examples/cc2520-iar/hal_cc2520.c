a08259191022caaf0571bda3885cae1051ef4173
Anthony Gelibert <anthony.gelibert@me.com>
2011-03-15 13:51:24 +0100

    Code now compliant with msp430-gcc

--- examples/cc2520-iar/hal_cc2520.c
+++ examples/cc2520-iar/hal_cc2520.c
@@ -30,8 +30,8 @@
 #define CC2520_SPI_WAIT_RXRDY()         st( while (!(CC2520_SPI_RX_IS_READY())); )
 
 // SPI interface control
-#define CC2520_SPI_BEGIN()                 P3OUT &= ~BIT0;  NOP(); NOP(); NOP(); 
-#define CC2520_SPI_END()                   NOP(); NOP(); NOP(); P3OUT |= BIT0; NOP(); NOP(); NOP();
+#define CC2520_SPI_BEGIN()                 P3OUT &= ~BIT0;  _NOP(); _NOP(); _NOP();
+#define CC2520_SPI_END()                   _NOP(); _NOP(); _NOP(); P3OUT |= BIT0; _NOP(); _NOP(); _NOP();
 
 
 /***********************************************************************************
@@ -1670,7 +1670,7 @@ void CC2520_REGWR24(uint8 addr, uint32 value)
   its documentation for any purpose.
 
   YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
-  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  PROVIDED ï¿½AS ISï¿½ WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
   INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
   NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
   TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
