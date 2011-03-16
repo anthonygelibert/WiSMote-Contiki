8b543c38bc57be2225cd3bd7cd5b1dbae4b23608
Anthony Gelibert <anthony.gelibert@me.com>
2011-03-16 12:27:08 +0100

    Final version of the SHT15UDP example - server part

--- tools/UDP/SHT15UDP.java
+++ tools/UDP/SHT15UDP.java
@@ -1,50 +1,77 @@
-/******************************************************************************
- * Copyright 2011, Anthony Gelibert <anthony.gelibert@me.com>                 *
- *                                                                            *
- *    Licensed under the Apache License, Version 2.0 (the "License");         *
- *    you may not use this file except in compliance with the License.        *
- *    You may obtain a copy of the License at                                 *
- *                                                                            *
- *        http://www.apache.org/licenses/LICENSE-2.0                          *
- *                                                                            *
- *    Unless required by applicable law or agreed to in writing, software     *
- *    distributed under the License is distributed on an "AS IS" BASIS,       *
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
- *    See the License for the specific language governing permissions and     *
- *    limitations under the License.                                          *
- ******************************************************************************/
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+/**
+ * Simple class listening on 192.168.1.1:1234 (UDP) for TMP/HR data from a SHT1x
+ * sensor.<br/>
+ * <b>Format of the data:</b> tttt-hhhh (where t is for temperature and h for relative
+ * humidity). We used the default precision (14 bits/12 bits) and a power supply of 3.5V.
+ *
+ * @author Plateforme Technologique de Valence - Anthony Gelibert
+ * @version March 16, 2011
+ */
 public class SHT15UDP
 {
-
+    /* UDP buffers */
+    /** UDP Buffer size. */
     private static final int BUFFER_SIZE = 10;
+    /** UDP Buffer. */
     private static final byte[] BUFFER = new byte[BUFFER_SIZE];
 
-    private SHT15UDP()
-    {
-    }
+    /* Temperature (14 bits / 3.5V) constants */
+    private static final double D1 = -39.7;
+    private static final double D2 = 0.01;
+
+    /* Relative humidity (12 bits) constants */
+    private static final double C1 = -2.0468;
+    private static final double C2 = 0.0367;
+    private static final double C3 = -0.0000015955;
+
+    private SHT15UDP(){}
 
     public static void main(final String[] args) throws IOException
     {
-        System.out.println("Hello !!!");
         /* UDP socket. */
         final DatagramPacket data = new DatagramPacket(BUFFER, BUFFER_SIZE);
-        final DatagramSocket socket = new DatagramSocket(1234, InetAddress.getByName("172.28.1.224"));
+        final DatagramSocket socket = new DatagramSocket(1234, InetAddress.getByName("192.168.1.1"));
         System.out.println("I wait tmp/hr data on UDP port 1234\n");
         while (true)
         {
             socket.receive(data);
             final String[] values = new String(data.getData()).split("-", 2);
-            final double tmp = -39.7 + 0.01 * Double.parseDouble(values[0]);
-            System.out.println("Current TMP is : " + tmp);
-            final int hrValue = Integer.parseInt(values[1]);
-            //final double hr =
-            System.out.println("Current HR is : "  );
+            try
+            {
+                final double tmp = D1 + D2 * Double.parseDouble(values[0]);
+                System.out.println("TMP: " + round(tmp) + "°C");
+            }
+            catch (final NumberFormatException ex)
+            {
+                System.err.println("TMP: N/A");
+            }
+            try
+            {
+                final double rh = Double.parseDouble(values[1]);
+                final double hr = C1 + C2 * rh + C3 * rh * rh;
+                System.out.println("HR:  " + round(hr) + "%");
+            }
+            catch (final NumberFormatException ex)
+            {
+                System.err.println("HR:  N/A");
+            }
+            System.out.println("-------------");
         }
+    }
+
+    /**
+     * Round a double to 2 decimal digits.
+     *
+     * @param d Original double.
+     * @return Rounded version.
+     */
+    static double round(final double d)
+    {
+        return Math.rint(d * 100.0) / 100.0;
     }
 }
