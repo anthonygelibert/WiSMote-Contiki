14e26a889270dedeb208a51bbb62c57aa31d5a4d
Anthony Gelibert <anthony.gelibert@me.com>
2011-04-11 16:42:58 +0200

    Add comments.

--- platform/wismote/cc2520/cc2520_util.h
+++ platform/wismote/cc2520/cc2520_util.h
@@ -44,11 +44,148 @@
 #ifndef __CC2520_UTIL_H__
 #define __CC2520_UTIL_H__
 
-uint8  CC2520_MEMWR(uint16 addr, uint16 count, uint8  *pData);
-uint8  CC2520_MEMWR8(uint16 addr, uint8 value);
-uint8  CC2520_MEMWR16(uint16 addr, uint16 value);
-uint8  CC2520_MEMWR24(uint16 addr, uint32 value);
+#include "contiki-conf.h"
 
+/*******************************************************************************
+* @fn      CC2520_MEMWR
+*
+* @brief   Write memory
+*
+* @param   uint16_t addr
+*          uint16_t count
+*          uint8_t  *pData
+*
+* @return  uint8_t - status byte
+*/
+uint8_t CC2520_MEMWR(uint16_t addr, uint16_t count, uint8_t  *pData);
+
+/*******************************************************************************
+* @fn      CC2520_MEMWR8
+*
+* @brief   Write memory 8 bits
+*
+* @param   uint16_t addr
+*          uint8_t value
+*
+* @return  uint8_t - status byte
+*/
+uint8_t CC2520_MEMWR8(uint16_t addr, uint8_t value);
+
+/*******************************************************************************
+* @fn      CC2520_MEMWR16
+*
+* @brief   Write memory 16 bits
+*
+* @param   uint16_t addr
+*          uint16_t value
+*
+* @return  uint8_t - status byte
+*/
+uint8_t CC2520_MEMWR16(uint16_t addr, uint16_t value);
+
+/*******************************************************************************
+* @fn      CC2520_MEMWR24
+*
+* @brief   Write memory 24 bits
+*
+* @param   uint16_t addr
+*          uint32_t value
+*
+* @return  uint8_t - status byte
+*/
+uint8_t CC2520_MEMWR24(uint16_t addr, uint32_t value);
+
+/*******************************************************************************
+* @fn      CC2520_MEMRD
+*
+* @brief   Read memory
+*
+* @param   uint16_t addr
+*          uint16_t count
+*          uint8_t  *pData
+*
+* @return  uint8_t - status byte
+*/
+uint8_t CC2520_MEMRD(uint16_t addr, uint16_t count, uint8_t  *pData);
+
+/******************************************************************************
+* @fn      CC2520_MEMRD8
+*
+* @brief   Read memory 8 bits
+*
+* @param   uint16_t addr
+*
+* @return  uint8_t - result
+*/
+uint8_t CC2520_MEMRD8(uint16_t addr);
+
+/*******************************************************************************
+* @fn      CC2520_MEMRD16
+*
+* @brief   Read memory 16 bits
+*
+* @param   uint16_t addr
+*
+* @return  uint16_t - result
+*/
+uint16_t CC2520_MEMRD16(uint16_t addr);
+
+/*******************************************************************************
+* @fn      CC2520_MEMRD24
+*
+* @brief   Read memory 24 bits
+*
+* @param   uint16_t addr
+*
+* @return  uint32_t - result
+*/
+uint32_t CC2520_MEMRD24(uint16_t addr);
+
+/***********************************************************************************
+* @fn      CC2520_REGRD
+*
+* @brief   Register read. Can only be started from addresses below 0x40
+*
+* @param  uint8_t addr - address
+*         uint8_t count - number of bytes
+*         uint8_t  *pValues - buffer to store result
+*
+* @return  uint8_t - status byte
+*/
+uint8_t CC2520_REGRD(uint8_t addr, uint8_t count, uint8_t  *pValues);
+
+/***********************************************************************************
+* @fn      CC2520_REGRD8
+*
+* @brief   Read one register byte
+*
+* @param  uint8_t addr - address
+*
+* @return  uint8_t - result
+*/
+uint8_t CC2520_REGRD8(uint8_t addr);
+
+/***********************************************************************************
+* @fn      CC2520_REGRD16
+*
+* @brief   Read two register bytes
+*
+* @param  uint8_t addr - address
+*
+* @return  uint16_t - result
+*/
+uint16_t CC2520_REGRD16(uint8_t addr);
+
+/***********************************************************************************
+* @fn      CC2520_REGRD24
+*
+* @brief   Read three register bytes
+*
+* @param  uint8_t addr - address
+*
+* @return  uint32_t - result
+*/
+uint32_t CC2520_REGRD24(uint8_t addr);
 #endif
 
 /** @} */
