bd834917562d4f5898db6b1ade08b48da0c3cd76
Anthony Gelibert <anthony.gelibert@me.com>
2011-06-08 15:54:48 +0200

    Pretty complete support of multiple sht15. It's an ugly commit.
    Signed-off-by: Anthony Gelibert <anthony.gelibert@me.com>

---
+++ examples/multi-sht15/multsht15-sensors.c
@@ -0,0 +1,12 @@
+#include <signal.h>
+
+#include "contiki-conf.h"
+#include "lib/sensors.h"
+
+#include "multsht15-sensors.h"
+#include "sensors/sht1x-sensor.h"
+
+NEW_SHT1X(upper_sensor, "SHT15 UPPER",6,4,6,5,6,1);
+
+NEW_SHT1X(lower_sensor, "SHT15 LOWER",6,4,6,5,6,0);
+
