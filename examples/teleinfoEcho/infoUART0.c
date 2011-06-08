ed2e3d632dda23391f92706852e83e9d3123d8c1
Anthony Gelibert <anthony.gelibert@me.com>
2011-06-08 17:21:30 +0200

    Add the "teleinfoEcho" example.
    Signed-off-by: Anthony Gelibert <anthony.gelibert@me.com>

---
+++ examples/teleinfoEcho/infoUART0.c
@@ -0,0 +1,71 @@
+/**
+ * \addtogroup msp430x5xx
+ * @{
+ */
+
+/**
+ * \file
+ *         Teleinfo on UART0 Echo with event example.
+ * \author
+ *         Anthony Gelibert <anthony.gelibert@lcis.grenoble-inp.fr>
+ * \date
+ *         June 8, 2011
+ */
+
+/*
+ * Copyright (c) 2011, LCIS/CTSYS.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* From MSP430-GCC */
+#include <stdio.h>
+
+/* From Contiki */
+#include "contiki.h"
+#include "dev/serial-line.h"
+
+/*----------------------------------------------------------------------------*/
+PROCESS(echoEvent_process, "Echo Event Process");
+AUTOSTART_PROCESSES(&echoEvent_process);
+/*----------------------------------------------------------------------------*/
+PROCESS_THREAD(echoEvent_process, ev, data)
+{
+  PROCESS_BEGIN();
+  while(1)
+  {
+    /* I wait for a specific situation:
+     *   - The event is of kind "serial_line".
+     *   - The buffer isn't NULL.
+     */
+    PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message && data != NULL);
+    printf("%s",(char *)data);
+  }
+  PROCESS_END();
+}
+/*----------------------------------------------------------------------------*/
+
+/** @} */
