f7476648425d162cebecb3f21e05116e9c9a287c
Anthony Gelibert <anthony.gelibert@me.com>
2011-09-27 12:54:41 +0200

    Compatible with MSP430 GCC 4.5.x

--- examples/ipv6/rpl-border-router/slip-bridge.c
+++ examples/ipv6/rpl-border-router/slip-bridge.c
@@ -42,7 +42,7 @@
 #include "net/uip.h"
 #include "net/uip-ds6.h"
 #include "dev/slip.h"
-#include "dev/uart1.h"
+#include "uart1.h"
 #include <string.h>
 
 #define UIP_IP_BUF        ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
@@ -119,33 +119,33 @@ output(void)
 }
 
 /*---------------------------------------------------------------------------*/
-#undef putchar
-int
-putchar(int c)
-{
-#define SLIP_END     0300
-  static char debug_frame = 0;
-
-  if(!debug_frame) {            /* Start of debug output */
-    slip_arch_writeb(SLIP_END);
-    slip_arch_writeb('\r');     /* Type debug line == '\r' */
-    debug_frame = 1;
-  }
-
-  /* Need to also print '\n' because for example COOJA will not show
-     any output before line end */
-  slip_arch_writeb((char)c);
-
-  /*
-   * Line buffered output, a newline marks the end of debug output and
-   * implicitly flushes debug output.
-   */
-  if(c == '\n') {
-    slip_arch_writeb(SLIP_END);
-    debug_frame = 0;
-  }
-  return c;
-}
+//#undef putchar
+//int
+//putchar(int c)
+//{
+//#define SLIP_END     0300
+//  static char debug_frame = 0;
+//
+//  if(!debug_frame) {            /* Start of debug output */
+//    slip_arch_writeb(SLIP_END);
+//    slip_arch_writeb('\r');     /* Type debug line == '\r' */
+//    debug_frame = 1;
+//  }
+//
+//  /* Need to also print '\n' because for example COOJA will not show
+//     any output before line end */
+//  slip_arch_writeb((char)c);
+//
+//  /*
+//   * Line buffered output, a newline marks the end of debug output and
+//   * implicitly flushes debug output.
+//   */
+//  if(c == '\n') {
+//    slip_arch_writeb(SLIP_END);
+//    debug_frame = 0;
+//  }
+//  return c;
+//}
 /*---------------------------------------------------------------------------*/
 const struct uip_fallback_interface rpl_interface = {
   init, output
